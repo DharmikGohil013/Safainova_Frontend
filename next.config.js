c3d4cbb2c9c2402e5d8702295b097f0708361b21
DHARMIKgohil013 <dharmikgohil395003@gmail.com>
2025-03-06 02:05:39 +0530

    add

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // No need for experimental.appDir in Next.js 14
-  // ...other options
+  output: 'export',
 };
-
 module.exports = nextConfig;
